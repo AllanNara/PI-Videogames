c64c2c195f91f367f123ecd7328d03ffb9fc6118
AllanNara <allannara@outlook.com>
2022-05-06 01:56:02 -0300

    get all videogames API

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -6,79 +6,49 @@ const { Videogame, Genre, Op } = require('../db')
 const axios = require('axios');
 
 router.get('/', async (req, res, next) => {
-    const { name } = req.query
+    const { name } = req.query;
     
     try {
-        const options = !name ?
-        { include: Genre, attributes: {exclude: ['rating', 'released', 'description']} } :
-        { include: Genre, attributes: {exclude: ['rating', 'released', 'description']}, where: {name: {[Op.substring]: name }} }
+        const options = name ? 
+        { include: Genre, where: {name: {[Op.substring]: name }} }
+        : { include: Genre };
 
-        const allDataBase = await Videogame.findAll(options)
+        const callDataBase = await Videogame.findAll(options);
+        const resultDataBase = callDataBase.map(elem => elem.toJSON());
 
-        const dataBase = [];
-        allDataBase.forEach(elem => {
-            let { id, name, isDataBase, image, genres } = elem.toJSON();
-            let allGenres = genres.map(e => e.name);
-            dataBase.push({
-                id,
-                name,
-                isDataBase,
-                image,
-                genres: allGenres
-            });
-        });
+        const callApi = !name ?
+        `https://api.rawg.io/api/games?key=${API_KEY}`
+        : `https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`;
 
-        const main = !name ?
-         await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`) 
-         : await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`);
-         
-        for(let i = 0; i < 4; i++) {
-            const nexts = await axios.get(main.data.next)
-            main.data.results = [...main.data.results, ...nexts.data.results]
+        let resultApi = [];
+        for(let i = 1; i < 6; i++) {
+            const nexts = await axios.get(`${callApi}&page=${i}`);
+            resultApi = [...resultApi, ...nexts.data.results];
+            if(!nexts.data.next) break;
         };
-        
-        const data = [];
-        main.data.results.forEach(elem => {
-            let allGenres = elem.genres.map(e => e.name)
-            data.push({
+
+        const final = [...resultApi, ...resultDataBase];
+        const dataFinal = [];
+        final.forEach(elem => {
+            let allGenres = elem.genres.map(e => e.name);
+            dataFinal.push({
                 id: elem.id,
                 name: elem.name,
-                isDataBase: false,
-                image: elem.background_image, 
-                genres: allGenres, 
-            });
+                isDataBase: !!elem.isDataBase,
+                image: elem.image ? elem.image : elem.background_image,
+                genres: allGenres
+            })
         });
 
-        const result = [...data, ...dataBase];
+        return !name ? 
+        res.json(dataFinal) 
+        : dataFinal.length ? 
+        res.json(dataFinal) : 
+        res.status(404).json({ERROR: "Game not found"});
 
-         return !name ?
-          res.json(result) 
-          : result.length ?
-           res.json(result) : res.status(404).json({ERROR: "Game not found"});
     } catch (error) {
         next(error)
     }
 });
 
 module.exports = router
-
-
-// GET /videogames:
-// Obtener un listado de los videojuegos
-// Debe devolver solo los datos necesarios para la ruta principal
-
-// GET /videogames?name="...":
-// Obtener un listado de las primeros 15 videojuegos que contengan la palabra ingresada como query parameter
-// Si no existe ningún videojuego mostrar un mensaje adecuado
-
-
-// Ruta principal: debe contener
-
-// [ ] Input de búsqueda para encontrar videojuegos por nombre
-// [ ] Área donde se verá el listado de videojuegos. Deberá mostrar su:
-// Imagen
-// Nombre
-// Géneros
-// [ ] Botones/Opciones para filtrar por género y por videojuego existente o agregado por nosotros
-// [ ] Botones/Opciones para ordenar tanto ascendentemente como descendentemente los videojuegos por orden alfabético y por rating
-// [ ] Paginado para ir buscando y mostrando los siguientes videojuegos, 15 juegos por pagina, mostrando los primeros 15 en la primer pagina.
