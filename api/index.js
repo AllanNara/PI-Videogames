c2cea6e49dadb5a06160ceb0e701ae8e97d35048
AllanNara <allannara@outlook.com>
2022-07-07 21:32:12 -0300

    force true

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const { preLoad } = require('./src/controllers')
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
     preLoad()
